196aba5652e133ec205751744705f60c629ea853
R B B <roshanbhave@gmail.com>
2016-03-21 16:17:34 +0000

    Get running instance

---
+++ testaws.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import boto
+import boto.ec2
+
+reg = next(x for x in boto.ec2.regions() if x.name=='us-west-1')
+conn = boto.ec2.EC2Connection(region=reg)
+reservations = conn.get_all_instances(filters={'instance-state-name': 'running'})
+instances = [ i for r in reservations for i in r.instances]
+print conn
+for i in instances:
+    print(i.__dict__)
+    break
+
+
